51561c8a5fc033f4757881fea354307461ed8c08
Ibo Sy <ibo@b9lab.com>
2021-02-20 01:20:25 +0100

    add video

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 const themeOptions = require('./theme-options');
+const remarkTypescript = require('remark-typescript');
 
 const pathPrefix = "/";
 
@@ -13,6 +14,8 @@ const getSidebarConfig = () => {
   return sidebarCategories = {...sidebarDefaults, ...sidebarContent};
 }
 
+const sidebarConfig = getSidebarConfig();
+
 const apolloDocsOptions = {
   ...themeOptions,
   root: __dirname,
@@ -21,6 +24,32 @@ const apolloDocsOptions = {
   siteName: 'Tezos Developer Portal Preview',
   sidebarCategories: getSidebarConfig(),
 }
+
+const apolloRemarkPluginConfig = require("gatsby-theme-apollo-docs/gatsby-config.js")({
+  ...apolloDocsOptions,
+  sidebarCategories: sidebarConfig
+});
+
+const apolloGatsbyRemarkPlugins = apolloRemarkPluginConfig.plugins.find(i => i.resolve == "gatsby-transformer-remark").options.plugins;
+
+let remarkPluginConfig = [
+  {
+    resolve: "gatsby-remark-embed-video",
+    options: {
+      width: "100%",
+      height: 400,
+      related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+      noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+    }
+  },
+  {
+    resolve: `gatsby-remark-katex`,
+    options: {
+      // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+      strict: `ignore`
+    }
+  }
+]
 
 module.exports = {
   pathPrefix: pathPrefix,
@@ -34,6 +63,18 @@ module.exports = {
     },
     {
       resolve: 'gatsby-transformer-remark',
-    }
+      options: {
+        plugins: remarkPluginConfig.concat(apolloGatsbyRemarkPlugins, ['gatsby-remark-autolink-headers', 'gatsby-remark-check-links'])
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: remarkPluginConfig.concat(apolloGatsbyRemarkPlugins),
+        remarkPlugins: [
+          [remarkTypescript, {wrapperComponent: 'MultiCodeBlock'}]
+        ]
+      }
+    },
   ]
 };
