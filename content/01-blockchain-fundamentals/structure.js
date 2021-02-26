03a9e301e16e8df0a4a960528bb6be408cc65020
Anil <anil@b9lab.com>
2021-02-26 15:47:41 +0100

    fix structure

--- content/01-blockchain-fundamentals/structure.js
+++ content/01-blockchain-fundamentals/structure.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const here = `${path.parse(__dirname).base}${path.sep}`;
 module.exports = [
-    `${here}01-introduction`,
-    `${here}02-getting-into-tezos`,
+    `${here}01-introduction-1`,
+    `${here}02-introduction-2`,
+    `${here}03-getting-into-tezos-1`,
+    `${here}04-getting-into-tezos-2`,
 ];
