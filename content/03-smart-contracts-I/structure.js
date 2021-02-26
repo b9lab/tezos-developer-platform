7bb2be9dc4982012d0c726e0c3bf98367a06ae29
Citlali Mora Catlett <citlali@b9lab.com>
2021-02-26 17:49:41 +0100

    included second exercise in SmartPy module

--- content/03-smart-contracts-I/structure.js
+++ content/03-smart-contracts-I/structure.js
@@ -3,5 +3,6 @@ const here = `${path.parse(__dirname).base}${path.sep}`;
 module.exports = [
     `${here}01-first-steps`,
     `${here}02-language`,
-    `${here}03-sample`,
+    `${here}03-exercise-1`,
+    `${here}04-exercise-2`,
 ];
