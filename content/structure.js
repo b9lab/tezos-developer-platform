1af13c8d6c3721b882b0a031aee97057a6fdcdd9
Citlali Mora Catlett <citlali@b9lab.com>
2021-03-08 15:47:54 +0100

    changed titles

--- content/structure.js
+++ content/structure.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "Blockchain Fundamentals": require("./01-blockchain-fundamentals/structure.js"),
+  "Tezos Introduction": require("./01-blockchain-fundamentals/structure.js"),
   "Getting to Know Tezos": require("./02-tezos-basics/structure.js"),
-  "Smart Contracts I": require("./03-smart-contracts-I/structure.js"),
-  "Smart Contracts II": require("./04-smart-contracts-II/structure.js"),
+  "Writing Smart Contracts": require("./03-smart-contracts-I/structure.js"),
+  "More Smart Contract Development": require("./04-smart-contracts-II/structure.js"),
 };
