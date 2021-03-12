2b685b9b11667827e4c252aa59032298cb01020f
Anil <anil@b9lab.com>
2021-03-12 17:54:53 +0100

    init intro taq

--- content/structure.js
+++ content/structure.js
@@ -3,4 +3,5 @@ module.exports = {
   "Getting to Know Tezos": require("./02-tezos-basics/structure.js"),
   "Writing Smart Contracts": require("./03-smart-contracts-I/structure.js"),
   "More Smart Contract Development": require("./04-smart-contracts-II/structure.js"),
+  "Developing Clients": require("./05-clients-I/structure.js"),
 };
