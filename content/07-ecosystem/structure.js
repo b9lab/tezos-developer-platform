ab5eb7d8d2d83fd4b959d5e164b27daecc5785e2
Anil <anil@b9lab.com>
2021-03-17 10:34:47 +0100

    init module 7

---
+++ content/07-ecosystem/structure.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const here = `${path.parse(__dirname).base}${path.sep}`;
+module.exports = [
+    `${here}01-fa2`,
+    `${here}02-oracle`,
+    `${here}03-beacon`,
+    `${here}04-dexchg`,
+];
--- content/structure.js
+++ content/structure.js
@@ -4,4 +4,6 @@ module.exports = {
   "Writing Smart Contracts": require("./03-smart-contracts-I/structure.js"),
   "More Smart Contract Development": require("./04-smart-contracts-II/structure.js"),
   "Developing Clients": require("./05-clients-I/structure.js"),
+  "Fetching more Blockchain data": require("./06-clients-II/structure.js"),
+  "Tezos Ecosystem": require("./07-ecosystem/structure.js")
 };
