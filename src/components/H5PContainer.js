df8384f2a8dc800a7e5e103421fe8ad8876af1a5
Gioele Bonifetto <gioele.bonifetto@gmail.com>
2021-03-19 15:59:38 +0100

    handles multiple H5P elements in the same page

--- src/components/H5PContainer.js
+++ src/components/H5PContainer.js
@@ -1,44 +1,52 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import {breakpoints} from 'gatsby-theme-apollo-core';
-//import { H5P } from 'h5p-standalone';
 
 export default class H5PContainer extends React.Component {
-  
 
   constructor(props) {
     super(props);
   }
 
   componentDidMount() {
-    console.log("component did mount");
-    
+    this.initH5p(this.props.locations);
+  }
+
+  async initH5p(contentLocations) {
     const { H5P } = require('h5p-standalone');
-    const el = document.getElementById('h5p_container');
-    
-    const h5pLocation = this.props.location;
 
     const options = {
       frameJs: '/js/h5p/frame.bundle.js',
       frameCss: '/css/h5p/h5p.css'
     };
 
-    const h5p = new H5P(el, h5pLocation, options);
-    
-    console.log("created");
+    for (var i = 0; i < contentLocations.length; i++) {
+      const element = document.getElementById('h5p_container_' + i);
+      const h5p = await new H5P(element, contentLocations[i], options);
+    }
   }
 
   render() {
+    const background = "#ffffff";
+    const width = "100%";
+    const minHeight = "200px";
+
     const H5PContainerWrapper = styled.div({
-      background: "#cccccc",
-      width: "100%",
-      minHeight: "300px"
+      background: background,
+      width: width,
+      minHeight: minHeight
     });
 
-    return (
-      <H5PContainerWrapper className="h5p-container-wrapper" id="h5p_container">
-        {this.props.children}  
-      </H5PContainerWrapper>
-    );
+    var containers = [];
+
+    for (var i = 0; i < this.props.locations.length; i++) {
+      var id = "h5p_container_" + i;
+      containers.push(
+        <H5PContainerWrapper className="h5p-container-wrapper" id={id}>
+          {this.props.children}  
+        </H5PContainerWrapper>
+      );
+    }
+
+    return containers;
   }
 }
