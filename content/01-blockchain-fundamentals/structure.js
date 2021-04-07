b6eb7d83b16f494b21c424291bf195ace1426361
Ibo Sy <ibo@b9lab.com>
2021-04-08 01:07:08 +0200

    update module one structure

--- content/01-blockchain-fundamentals/structure.js
+++ content/01-blockchain-fundamentals/structure.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const here = `${path.parse(__dirname).base}${path.sep}`;
 module.exports = [
-  `${here}01-introduction-1`,
   `${here}00-landingpage`,
+  `${here}01-introduction-1`,
   `${here}02-introduction-2`,
   `${here}03-getting-into-tezos-1`,
   `${here}04-getting-into-tezos-2`
