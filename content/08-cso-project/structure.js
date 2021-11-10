ba2746649ec2772bbfef8e4a115b2fca0cffc5dd
Citlali Mora Catlett <citlali@b9lab.com>
2021-11-10 01:43:53 +0100

    grammar and typos, naming of file

--- content/08-cso-project/structure.js
+++ content/08-cso-project/structure.js
@@ -2,7 +2,7 @@ const path = require('path');
 const here = `${path.parse(__dirname).base}${path.sep}`;
 module.exports = [
   `${here}00-landingpage`,
-  `${here}01-intro-what-is-a-cso`,
+  `${here}01-intro-what-is-a-rolling-safe`,
   `${here}02-smart-contract-implementation-1`,
   `${here}03-smart-contract-implementation-2`,
   `${here}04-platform-specification`,
